b695f812d6711d1f3b00c70fb02a71c3cabfd685
Aru <vine9151@gmail.com>
2024-09-08 12:27:39 +0800

    release: first commit

---
+++ mcu_solder_plate/main.c
@@ -0,0 +1,60 @@
+/**
+ * NOTE      - main.c
+ * Author    - Chae Lee-Jin
+ * 
+ * Created   - 2024.09.01
+ * Github    - https://github.com/aruyu
+ * Contact   - vine9151@gmail.com
+ */
+
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "src/lib/ssd1306.h"
+
+
+ISR(INT0_vect);
+ISR(INT1_vect);
+
+
+
+int main(void)
+{
+	SSD1306_Init(SSD1306_ADDR);                      // 0x3C
+
+	SSD1306_ClearScreen();                           // clear screen
+	SSD1306_DrawLine(0, MAX_X, 4, 4);                // draw line
+	SSD1306_SetPosition(7, 1);                       // set position
+	SSD1306_DrawString("* MCU SOLDER PLATE *");      // draw string
+	SSD1306_DrawLine(0, MAX_X, 18, 18);              // draw line
+	SSD1306_SetPosition(40, 3);                      // set position
+	SSD1306_DrawString("LEE JIN");                   // draw string
+	SSD1306_SetPosition(53, 5);                      // set position
+	SSD1306_DrawString("2024");                      // draw string
+	SSD1306_UpdateScreen(SSD1306_ADDR);              // update
+
+
+    /* Replace with your application code */
+    while (1) 
+    {
+		_delay_ms(1000);
+		SSD1306_InverseScreen(SSD1306_ADDR);
+
+		_delay_ms (1000);
+		SSD1306_NormalScreen(SSD1306_ADDR);
+    }
+}
+
+
+
+ISR(INT0_vect)
+{
+}
+
+ISR(INT1_vect)
+{
+}
